948f61585cbc2cc2862d53b0caf1c3d00e43f527
Safnaj <safnaj99@gmail.com>
2019-05-13 15:11:15 +0530

    Dashboard UI

---
+++ Dashboard.php
@@ -0,0 +1,76 @@
+<?php include_once("includes/header.php"); ?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-4">
+            <div class="card mx-auto">
+                <img class="card-img-top mx-auto" style="width: 50%;" src="images/avatar.png" alt="Card image cap">
+                <div class="card-body">
+                    <h4 class="card-title">Profile Info</h4>
+                    <p class="card-text"><i class="fa fa-user">&nbsp;</i> Ahamed Safnaj</p>
+                    <p class="card-text">Admin</p>
+                    <p class="card-text">Last Login : xxxx-xx-xx</p>
+                    <a href="#" class="btn btn-primary">Edit Profile</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-8">
+            <div class="jumbotron" style="width: 100%; height: 100%;">
+                <h2>Welcome Admin</h2>
+                <div class="row">
+                    <div class="col-sm-6">
+                        <iframe src="http://free.timeanddate.com/clock/i6rcpp5d/n389/szw160/szh160/cf100/hnce1ead6" frameborder="0" width="160" height="160"></iframe>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title">New Orders</h5>
+                                <p class="card-text">Make Invoices and Create New Orders.</p>
+                                <a href="#" class="btn btn-primary">New Orders</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<br>
+<br>
+<div class="container">
+    <div class="row">
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Categories</h5>
+                    <p class="card-text">Here You Can Manage Your Categories</p>
+                    <a href="#" class="btn btn-primary">Add</a>
+                    <a href="#" class="btn btn-primary">Manage</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Brands</h5>
+                    <p class="card-text">Here You Can Manage Your Brands</p>
+                    <a href="#" class="btn btn-primary">Add</a>
+                    <a href="#" class="btn btn-primary">Manage</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Products</h5>
+                    <p class="card-text">Here You Can Manage Your Products</p>
+                    <a href="#" class="btn btn-primary">Add</a>
+                    <a href="#" class="btn btn-primary">Manage</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<br>
+</body>
+</html>
